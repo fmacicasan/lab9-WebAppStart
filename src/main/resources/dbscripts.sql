664e7dfcbc67f34805d47954a5f5894bedbcb403
flo <flo.macicasan@gmail.com>
2017-02-14 16:44:53 +0200

    step 7: create db scripts & run them in pgadmin

--- src/main/resources/dbscripts.sql
+++ src/main/resources/dbscripts.sql
@@ -1,16 +1,9 @@
-CREATE TABLE someTable (
-    idSomeTable bigserial NOT NULL PRIMARY KEY,
-    field1 character(20),
-    field2 int,
-    field3 DATE not null default CURRENT_DATE,
-    field4 numeric(7,2)
+CREATE TABLE article (
+    id bigserial NOT NULL PRIMARY KEY,
+    link character(300) NOT NULL,
+    domain character(40),
+    summary character(120),
+    date DATE NOT NULL default CURRENT_DATE
 );
 
-CREATE TABLE someOtherTable (
-    idSomeOtherTable bigserial NOT NULL PRIMARY KEY,
-    field1 character(20),
-    field2 int NOT NULL REFERENCES someTable(idSomeTable)
-);
-
-INSERT INTO sometable( field1, field2, field4) VALUES ('value1', 10, 5.2);
-INSERT INTO someOtherTable( field1, field2) VALUES ('value2', 1);
+INSERT INTO article( link, domain, summary, date) VALUES ('https://medium.freecodecamp.com/things-i-wish-someone-had-told-me-when-i-was-learning-how-to-code-565fc9dcb329#.k6upfeizh', 'learning', 'The path from “not working” to “working” might not be obvious, but with patience you can usually find it', '2016-12-20');
